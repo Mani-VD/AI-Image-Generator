e8d24a11b6f13ce8af47c9b5789257b32628fdb5
mani-vd <manikandanvd123@gmail.com>
2023-12-15 09:21:57 +0530

    First commit

---
+++ src/app/api/stablediffusion/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server';
+import Replicate from 'replicate';
+
+export const POST = async function(req: any, res: any) {
+  console.log(typeof req,typeof res,'types')
+  let req_val=await req.json();
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method not allowed' });
+    return;
+  }
+  // console.log(req,"valll")
+  const { value } = req_val;
+  
+
+  try {
+    const replicate = new Replicate({
+      auth: process.env.REPLICATE_API_TOKEN,
+    });
+
+    const output:any = await replicate.run(
+      "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+      {
+        input: {
+          prompt: value,
+          image_dimensions: "512x512",
+          num_inference_steps: 12,
+          num_outputs: 1,
+          guideance_scale: 3.5,
+          scheduler: "K_EULER",
+        },
+      },
+    );
+    console.log(output,'ot')
+    return NextResponse.json({output:output})
+  
+  } catch (error) {
+    // console.error(error);
+    NextResponse.json({ message: 'Internal server error' });
+  }
+};
+
